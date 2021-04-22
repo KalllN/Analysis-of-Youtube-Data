bf588e2d90e42709a2c389859786903972b3fba5
elloise <49631121+KalllN@users.noreply.github.com>
2021-04-22 17:12:59 +0530

    Update video_cat.py

--- data/video_cat.py
+++ data/video_cat.py
@@ -14,3 +14,11 @@ category['category_id'] = category['category_id'].astype('int64')
 
 #Instead of maintaining two different datasets, I merged both into one by adding categories into the original 'data' dataset.
 data = pd.merge(data, category, on = 'category_id', how = 'inner')
+
+data['trending_date'] = pd.to_datetime(data['trending_date'], 
+                   format = '%y.%d.%m')
+
+data.insert(4, 'publish_date', 
+            data['publish_time'].dt.date)
+
+data['publish_time'] = data['publish_time'].dt.time
